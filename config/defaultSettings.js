d3193c8205cbb98e30cec370f9e73d249eab411b
dat_dm_vn <datdm@ascendsgroup.com>
2019-10-31 14:09:03 +0700

    init project

---
+++ config/defaultSettings.js
@@ -0,0 +1,32 @@
+export default {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export const customSetting = {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'topmenu',
+  contentWidth: 'Fixed',
+  fixedHeader: true,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  menu: {
+    locale: true
+  },
+  title: 'We Pro',
+  pwa: false,
+  iconfontUrl: ''
+};
