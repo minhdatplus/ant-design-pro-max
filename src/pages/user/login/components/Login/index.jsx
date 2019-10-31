743fed96157a82822396ec9a0605f0cdf08cbd61
dat_dm_vn <datdm@ascendsgroup.com>
2019-10-31 15:37:00 +0700

    update config and language

--- src/pages/user/login/components/Login/index.jsx
+++ src/pages/user/login/components/Login/index.jsx
@@ -8,142 +8,145 @@ import LoginTab from './LoginTab';
 import styles from './index.less';
 
 class Login extends Component {
-  static Tab = LoginTab;
-  static Submit = LoginSubmit;
-  static defaultProps = {
-    className: '',
-    defaultActiveKey: '',
-    onTabChange: () => {},
-    onSubmit: () => {},
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      type: props.defaultActiveKey,
-      tabs: [],
-      active: {},
+    static Tab = LoginTab;
+    static Submit = LoginSubmit;
+    static defaultProps = {
+        className: '',
+        defaultActiveKey: '',
+        onTabChange: () => {
+        },
+        onSubmit: () => {
+        },
     };
-  }
 
-  componentDidMount() {
-    const { form, onCreate } = this.props;
-
-    if (onCreate) {
-      onCreate(form);
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: props.defaultActiveKey,
+            tabs: [],
+            active: {},
+        };
     }
-  }
 
-  onSwitch = type => {
-    this.setState(
-      {
-        type,
-      },
-      () => {
-        const { onTabChange } = this.props;
+    componentDidMount() {
+        const { form, onCreate } = this.props;
 
-        if (onTabChange) {
-          onTabChange(type);
+        if (onCreate) {
+            onCreate(form);
         }
-      },
-    );
-  };
-  getContext = () => {
-    const { form } = this.props;
-    const { tabs = [] } = this.state;
-    return {
-      tabUtil: {
-        addTab: id => {
-          this.setState({
-            tabs: [...tabs, id],
-          });
-        },
-        removeTab: id => {
-          this.setState({
-            tabs: tabs.filter(currentId => currentId !== id),
-          });
-        },
-      },
-      form: { ...form },
-      updateActive: activeItem => {
-        const { type = '', active = {} } = this.state;
+    }
 
-        if (active[type]) {
-          active[type].push(activeItem);
-        } else {
-          active[type] = [activeItem];
+    onSwitch = type => {
+        this.setState(
+            {
+                type,
+            },
+            () => {
+                const { onTabChange } = this.props;
+
+                if (onTabChange) {
+                    onTabChange(type);
+                }
+            },
+        );
+    };
+    getContext = () => {
+        const { form } = this.props;
+        const { tabs = [] } = this.state;
+        return {
+            tabUtil: {
+                addTab: id => {
+                    this.setState({
+                        tabs: [...tabs, id],
+                    });
+                },
+                removeTab: id => {
+                    this.setState({
+                        tabs: tabs.filter(currentId => currentId !== id),
+                    });
+                },
+            },
+            form: { ...form },
+            updateActive: activeItem => {
+                const { type = '', active = {} } = this.state;
+
+                if (active[type]) {
+                    active[type].push(activeItem);
+                } else {
+                    active[type] = [activeItem];
+                }
+
+                this.setState({
+                    active,
+                });
+            },
+        };
+    };
+    handleSubmit = e => {
+        e.preventDefault();
+        const { active = {}, type = '' } = this.state;
+        const { form, onSubmit } = this.props;
+        const activeFields = active[type] || [];
+
+        if (form) {
+            form.validateFields(
+                activeFields,
+                {
+                    force: true,
+                },
+                (err, values) => {
+                    if (onSubmit) {
+                        onSubmit(err, values);
+                    }
+                },
+            );
         }
+    };
 
-        this.setState({
-          active,
+    render() {
+        const { className, children } = this.props;
+        const { type, tabs = [] } = this.state;
+        const TabChildren = [];
+        const otherChildren = [];
+        React.Children.forEach(children, child => {
+            if (!child) {
+                return;
+            }
+
+            if (child.type.typeName === 'LoginTab') {
+                TabChildren.push(child);
+            } else {
+                otherChildren.push(child);
+            }
         });
-      },
-    };
-  };
-  handleSubmit = e => {
-    e.preventDefault();
-    const { active = {}, type = '' } = this.state;
-    const { form, onSubmit } = this.props;
-    const activeFields = active[type] || [];
-
-    if (form) {
-      form.validateFields(
-        activeFields,
-        {
-          force: true,
-        },
-        (err, values) => {
-          if (onSubmit) {
-            onSubmit(err, values);
-          }
-        },
-      );
+        return (
+            <LoginContext.Provider value={this.getContext()}>
+                <div className={classNames(className, styles.login)}>
+                    <Form onSubmit={this.handleSubmit}>
+                        {tabs.length ? (
+                            <React.Fragment>
+                                <Tabs
+                                    animated={false}
+                                    className={styles.tabs}
+                                    activeKey={type}
+                                    onChange={this.onSwitch}
+                                >
+                                    {TabChildren}
+                                </Tabs>
+                                {otherChildren}
+                            </React.Fragment>
+                        ) : (
+                            children
+                        )}
+                    </Form>
+                </div>
+            </LoginContext.Provider>
+        );
     }
-  };
-
-  render() {
-    const { className, children } = this.props;
-    const { type, tabs = [] } = this.state;
-    const TabChildren = [];
-    const otherChildren = [];
-    React.Children.forEach(children, child => {
-      if (!child) {
-        return;
-      }
-
-      if (child.type.typeName === 'LoginTab') {
-        TabChildren.push(child);
-      } else {
-        otherChildren.push(child);
-      }
-    });
-    return (
-      <LoginContext.Provider value={this.getContext()}>
-        <div className={classNames(className, styles.login)}>
-          <Form onSubmit={this.handleSubmit}>
-            {tabs.length ? (
-              <React.Fragment>
-                <Tabs
-                  animated={false}
-                  className={styles.tabs}
-                  activeKey={type}
-                  onChange={this.onSwitch}
-                >
-                  {TabChildren}
-                </Tabs>
-                {otherChildren}
-              </React.Fragment>
-            ) : (
-              children
-            )}
-          </Form>
-        </div>
-      </LoginContext.Provider>
-    );
-  }
 }
 
-Object.keys(LoginItem).forEach(item => {
-  Login[item] = LoginItem[item];
-});
+Object.keys(LoginItem)
+    .forEach(item => {
+        Login[item] = LoginItem[item];
+    });
 export default Form.create()(Login);
